308a60828f67b8610ff471b5b1d78dfae530ae4a
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2025-08-07 20:47:22 -0400

    update api routers variable

--- app/api/v1/api_v1.py
+++ app/api/v1/api_v1.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from app.resources.utilities import endpoints
-from app.resources.entities import endpoints
-from app.resources.tags import endpoints
+from app.resources.tags import endpoints as tags_endpoints
+from app.resources.entities import endpoints as entities_endpoints
+from app.resources.utilities import endpoints as utilities_endpoints
 
 
 api_router = APIRouter(prefix="/v1")
 
-api_router.include_router(endpoints.router)
-api_router.include_router(endpoints.router)
-api_router.include_router(endpoints.router)
+api_router.include_router(tags_endpoints.router)
+api_router.include_router(entities_endpoints.router)
+api_router.include_router(utilities_endpoints.router)
