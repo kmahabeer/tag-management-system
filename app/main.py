659c3d38bb9f3fe79941d0deb9bd14edafb7f4d0
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2025-08-23 01:32:09 +0000

    (backend): add CORS middleware to allow testing with vite dev server

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import sqlalchemy_engine
 from app.api.v1.api_v1 import api_router
 from app.core.config import settings
@@ -20,6 +21,18 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title=settings.PROJECT_NAME, root_path="/api", lifespan=lifespan)
 
 app.include_router(api_router)
+
+origins = [
+    "http://localhost:5173",   # Vite dev server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
