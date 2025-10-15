52c6b9a89d94d6c7ce41fd5c50a958609a05b8a0
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2025-10-15 14:41:41 +0000

    update devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -10,5 +10,9 @@ apt -y autoclean
 apt -y clean
 rm -rf /var/lib/apt/lists/*
 
+# Ensure shared dev network exists
+docker network inspect tag-management-system_dev_net >/dev/null 2>&1 || \
+  docker network create tag-management-system_dev_net
+
 # # Start backend API
 # uvicorn app.main:app --reload # Needs a way to wait until uv installs all packages
